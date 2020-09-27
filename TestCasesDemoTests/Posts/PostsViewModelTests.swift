aedcd240b2729c5ce54a1153e21424b1ed33543f
Shankar B S <shankarbs@MacBook-Pro.domain.name>
2020-09-27 18:45:39 +0530

    added source code

---
+++ TestCasesDemoTests/Posts/PostsViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  PostsViewModelTests.swift
+//  TestCasesDemoTests
+//
+//  Created by Shankar B S on 27/09/20.
+//  Copyright © 2020 Slicode. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import TestCasesDemo
+
+class PostsViewModelTests: XCTestCase {
+    
+}
